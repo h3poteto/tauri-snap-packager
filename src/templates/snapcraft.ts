b3f15cc5508fd6b351a6385fee70e4b9e8d23422
AkiraFukushima <h3.poteto@gmail.com>
2024-01-29 21:21:14 +0900

    Update core20

--- src/templates/snapcraft.ts
+++ src/templates/snapcraft.ts
@@ -1,33 +1,25 @@
-import { ISnapcraftYaml } from '../types/snapcraft';
+import { ISnapcraftYaml } from "../types/snapcraft";
 
 export const BASIC_SNAPCRAFT_YAML = <ISnapcraftYaml>{
-  name: 'placeholder-name',
-  base: 'core18',
-  version: 'placeholder-version',
-  summary: 'Tauri app.',
-  description: 'Awesome Tauri app.',
+  name: "placeholder-name",
+  base: "core20",
+  version: "placeholder-version",
+  summary: "placeholder-summary",
+  description: "placeholder-description",
 
-  grade: 'devel',
-  confinement: 'strict',
+  grade: "stable",
+  confinement: "strict",
 
   apps: {},
 
   parts: {
-    'dump-binary': {
-      plugin: 'dump',
-      source: 'release',
-      'source-type': 'local',
-      stage: [
-        'lib',
-        'icons'
-      ],
-      prime: [
-        'lib',
-        'icons'
-      ],
-      'stage-packages': [
-        'libc6'
-      ]
-    }
-  }
-}
+    "dump-binary": {
+      plugin: "dump",
+      source: "release",
+      "source-type": "local",
+      stage: ["lib", "icons"],
+      prime: ["lib", "icons"],
+      "stage-packages": ["libc6"],
+    },
+  },
+};
--- src/types/tauri-conf.ts
+++ src/types/tauri-conf.ts
@@ -2,12 +2,14 @@ export interface ITauriConf {
   package: {
     productName: string;
     version: string;
-  }
+  };
   tauri: {
     bundle: {
       icon: string[];
       targets: string;
       identifier: string;
-    }
-  }
+      shortDescription: string;
+      longDescription: string;
+    };
+  };
 }
